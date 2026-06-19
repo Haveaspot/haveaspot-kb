bac66ef091294efda3867a26502800bac6861c0e
Haveaspot <services@haveaspot.com>
2026-06-19 10:55:07 +0100

    feat: match hero heading size and weight to About site section heading

--- astro.config.mjs
+++ astro.config.mjs
@@ -42,7 +42,7 @@ export default defineConfig({
                     tag: 'link',
                     attrs: {
                         rel: 'stylesheet',
-                        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
+                        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap',
                     },
                 },
                 {
